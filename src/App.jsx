e0075890ba186eae48ea187c8fb712497bb4ab08
MatiasGOrtega <mat-1998-ort@hotmail.com>
2025-06-30 19:43:25 -0300

    feat: implement theme context and routing structure; remove unused CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { AuthContextProvider } from './index'
+import styled, { ThemeProvider } from 'styled-components'
+import { AuthContextProvider, Dark, Light } from './index'
+import AppRoutes from './routers/routes'
+import { createContext, useState } from 'react'
+import { Device } from './index'
+
+export const ThemeContext = createContext(null);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeuse, setThemeuse] = useState('dark');
+  const theme = themeuse === "light" ? "light" : "dark";
+  const themeStyle = theme === "light" ? Light : Dark;
+
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <AuthContextProvider>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </AuthContextProvider>
+    <ThemeContext.Provider value={{ theme, setThemeuse }}>
+      <ThemeProvider theme={themeStyle}>
+        <AuthContextProvider>
+          <Container className={sidebarOpen ? 'sidebar-open' : ''}>
+            <section className='content-sidebar'>
+              {/* Sidebar content can go here */}
+              SIDE BAR
+            </section>
+            <section className='content-menuburger'>
+              MENU BURGER
+            </section>
+            <section className='content-routes'>
+              <AppRoutes />
+            </section>
+          </Container>
+        </AuthContextProvider>
+      </ThemeProvider>
+    </ThemeContext.Provider>
   )
 }
 
+const Container = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  background-color: ${({ theme }) => theme.bgtotal};
+
+  .content-sidebar {
+    display: none;
+  }
+
+  .content-menuburger {
+    display: block;
+    position: absolute;
+    left: 20px;
+  }
+
+  @media ${Device.tablet} {
+    grid-template-columns: 65px 1fr;
+
+    &.sidebar-open {
+      grid-template-columns: 250px 1fr;
+    }
+
+    .content-sidebar {
+      display: initial;
+    }
+
+    .content-menuburger {
+      display: none;
+    }
+  }
+
+  .content-routes {
+    grid-column: 1;
+    width: 100%;
+
+    @media ${Device.tablet} {
+      grid-column: 2;
+    }
+  }
+`;
 export default App
