14b110618885877ca75f565e85e0cb0ee9e5a5e7
MatiasGOrtega <mat-1998-ort@hotmail.com>
2025-06-30 19:48:35 -0300

    feat: refactor Home component to use styled-components for layout and styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
+import styled from "styled-components"
+
 function Home() {
   return (
-    <div>Home</div>
+    <Container>
+      <h1>Home</h1>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  overflow: hidden;
+  background-color: ${({ theme }) => theme.bgtotal};
+  color: ${({ theme }) => theme.text};
+  width: 100%;
+`
 
 export default Home
